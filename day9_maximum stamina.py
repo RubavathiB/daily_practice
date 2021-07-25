ba48a83d891c524b62675a02fcc4bb7a963c3c96
RubavathiB <ruba.babu107@gmail.com>
2021-07-25 17:24:58 +0530

    add maximum stamina problem

---
+++ day9_maximum stamina.py
@@ -0,0 +1,17 @@
+myinp=int(input())
+arr=list(map(int,input().split()))
+myarr=[]
+for i in range(myinp-1):
+    checkmax=arr[i]
+    eval_out=str(arr[i])
+    for j in range(i+1,myinp):
+        if arr[j]>checkmax:
+            checkmax=arr[j]
+            eval_out+='^'+str(arr[j])
+    
+    myarr.append(eval(eval_out))
+
+
+myarr.append(arr[-1])
+
+print(max(myarr)
